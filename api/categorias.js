fdf8f246869dbffa010a88edb482f6b37fa11192
gerald-M14 <geraldhymessu7@gmail.com>
2025-10-15 04:02:05 -0500

    Subiendo productos

--- api/categorias.js
+++ api/categorias.js
@@ -1,6 +1,7 @@
 import pool from "../db.js";   // usa el mismo pool global
 import { applyCORS } from "../_cors.js";
 
+
 export default async function handler(req, res) {
   if (applyCORS(req, res, { allow: ["*"], methods: "GET,OPTIONS" })) return;
   if (req.method !== "GET") return res.status(405).json({ error: "method not allowed" });
