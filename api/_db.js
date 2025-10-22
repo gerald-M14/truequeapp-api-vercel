ca1c3f42a62d09e718e9abdfc693ff2d88b2b99c
gerald-M14 <geraldhymessu7@gmail.com>
2025-10-21 20:29:37 -0500

    Subiendo productos

--- api/_db.js
+++ api/_db.js
@@ -9,7 +9,7 @@ if (!global._pool) {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: 3306,
+    port: 20865,
     ssl: { rejectUnauthorized: false },
     waitForConnections: true,
     connectionLimit: 5,
